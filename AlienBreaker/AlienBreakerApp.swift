0e23650b636f2f690aefaca73e457b0390d310e2
Holger Hinzberg <holger@hinzberg.de>
2025-09-01 05:38:37 +0200

    Initial Commit

---
+++ AlienBreaker/AlienBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlienBreakerApp.swift
+//  AlienBreaker
+//
+//  Created by Holger Hinzberg on 01.09.25.
+//
+
+import SwiftUI
+
+@main
+struct AlienBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlienBreakerTests/AlienBreakerTests.swift
@@ -0,0 +1,17 @@
+//
+//  AlienBreakerTests.swift
+//  AlienBreakerTests
+//
+//  Created by Holger Hinzberg on 01.09.25.
+//
+
+import Testing
+@testable import AlienBreaker
+
+struct AlienBreakerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
